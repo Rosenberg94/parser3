5bde21c564bef14fd4173e48b9bd6a93f54488aa
Rosenberg94 <chebotnikovalexander@gmail.com>
2022-06-01 17:13:25 +0300

    db

---
+++ database/factories/CvFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cv>
+ */
+class CvFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'address' => $this->faker->unique()->safeEmail(),
+            'education' => Str::random(10),
+            'work_experience' =>  Str::random(20),
+        ];
+    }
+
+
+}
